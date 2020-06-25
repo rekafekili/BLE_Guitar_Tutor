d3dc4d05356d597b5056e843f6ef1b79c0e74405
rekafekili <rekafekili@gmail.com>
2020-06-25 23:25:07 +0900

    Test Strings into MainActivity.xml

--- app/src/main/java/com/example/ble_guitar_tutor/MainActivity.java
+++ app/src/main/java/com/example/ble_guitar_tutor/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements PermissionListene
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         checkBluetoothActive();
     }
 
